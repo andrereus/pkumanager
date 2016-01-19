1f77610e13dd7ff6e5a489870216bbc6e863226f
Andre Reus <andrereus@users.noreply.github.com>
2016-01-19 10:23:01 +0100

    Add sum

--- js/home.js
+++ js/home.js
@@ -10,6 +10,9 @@ function addZero(i) {
 if (localStorage.getItem("day") !== null) {
     var list = JSON.parse(localStorage.getItem("day"));
     var date;
+    var phe = 0;
+    var prot = 0;
+    var kcal = 0;
 
     var table = "<thead><tr><th>" +
         "Description</th><th>" +
@@ -22,13 +25,23 @@ if (localStorage.getItem("day") !== null) {
 
         table += "<tr><td>" +
             // addZero(date.getHours()) + ":" + addZero(date.getMinutes()) + " - " +
-            list[i].wg + "&nbsp;g " + list[i].desc + "</td><td class=\"nowrap\">" +
+            list[i].wg.toFixed(2).replace(/\.?0+$/, "") + "&nbsp;g " +
+            list[i].desc + "</td><td class=\"nowrap\">" +
             list[i].phe.toFixed(2).replace(/\.?0+$/, "") + " mg</td><td class=\"nowrap\">" +
             list[i].prot.toFixed(2).replace(/\.?0+$/, "") + " g</td><td>" +
             list[i].kcal.toFixed(2).replace(/\.?0+$/, "") + " kcal</td></tr>";
+
+        phe += list[i].phe;
+        prot += list[i].prot;
+        kcal += list[i].kcal;
     }
 
-    table += "</tbody><br><p>Reset list under <a href=\"settings.html\">Settings</a>.</p>";
+    table += "<tr><td>" +
+        "Sum</td><td class=\"nowrap\">" +
+        phe.toFixed(2).replace(/\.?0+$/, "") + " mg</td><td class=\"nowrap\">" +
+        prot.toFixed(2).replace(/\.?0+$/, "") + " g</td><td>" +
+        kcal.toFixed(2).replace(/\.?0+$/, "") + " kcal</td></tr>" +
+        "</tbody><br><p>Reset list under <a href=\"settings.html\">Settings</a>.</p>";
     today.innerHTML = table;
 } else {
     var empty = "<tbody><tr><td>No food added.</td></tr></tbody>";
