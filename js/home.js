4d2c716f80973616360bc10a96242a8900d0d42a
Andre Reus <andrereus@users.noreply.github.com>
2016-02-04 16:16:18 +0100

    Beta info

--- js/home.js
+++ js/home.js
@@ -1,14 +1,5 @@
 /* Initialize */
 var today = document.getElementById("today");
-var beta = document.getElementById("beta");
-
-function addListener(element, event, funct) {
-    if (element.addEventListener) {
-        return element.addEventListener(event, funct);
-    } else if (element.attachEvent) {
-        return element.attachEvent(event, funct);
-    }
-}
 
 /* Food list */
 if (localStorage.getItem("day") !== null) {
@@ -47,11 +38,4 @@ if (localStorage.getItem("day") !== null) {
 } else {
     var empty = "<tbody><tr><td><a href=\"add.html\">Add food</a> to see it here.</td></tr></tbody>";
     today.innerHTML = empty;
-}
-
-/* Beta version */
-addListener(beta, "click", info);
-
-function info() {
-    alert("This app is a beta version. It is recommended to reset the app (under Settings) from time to time during this period.");
 }
