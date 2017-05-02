c8f6c053b37b8fa00df5c0f9eb7915275d896dd0
Andre Reus <andrereus@users.noreply.github.com>
2017-05-02 14:31:27 +0200

    Firebase

--- js/login.js
+++ js/login.js
@@ -1,30 +1,44 @@
-var provider = new firebase.auth.GoogleAuthProvider();
+function toggleSignIn() {
+    if (!firebase.auth().currentUser) {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithRedirect(provider);
+    } else {
+    firebase.auth().signOut();
+    }
+    document.getElementById('quickstart-sign-in').disabled = true;
+}
 
-firebase.auth().signInWithRedirect(provider);
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    alert("User is signed in.");
-  } else {
-    // No user is signed in.
-  }
-});
+function initApp() {
+    firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+        var token = result.credential.accessToken;
+        document.getElementById('quickstart-oauthtoken').textContent = token;
+    } else {
+        document.getElementById('quickstart-oauthtoken').textContent = 'null';
+    }
+    var user = result.user;
+    }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+    if (errorCode === 'auth/account-exists-with-different-credential') {
+        alert('You have already signed up with a different auth provider for that email.');
+    } else {
+        console.error(error);
+    }
+    });
+    firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+    } else {
+        document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
+    }
+    document.getElementById('quickstart-sign-in').disabled = false;
+    });
+    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+}
+window.onload = function() {
+    initApp();
+};
