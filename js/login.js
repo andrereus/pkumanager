fea38071eb5622e84fa3f9422bacdac2dc0e50a1
Andre Reus <andrereus@users.noreply.github.com>
2017-05-02 14:38:02 +0200

    Format

--- js/login.js
+++ js/login.js
@@ -1,44 +1,51 @@
 function toggleSignIn() {
     if (!firebase.auth().currentUser) {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/plus.login');
-    firebase.auth().signInWithRedirect(provider);
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/plus.login');
+        firebase.auth().signInWithRedirect(provider);
     } else {
-    firebase.auth().signOut();
+        firebase.auth().signOut();
     }
+
     document.getElementById('quickstart-sign-in').disabled = true;
 }
 
 function initApp() {
     firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-        var token = result.credential.accessToken;
-        document.getElementById('quickstart-oauthtoken').textContent = token;
-    } else {
-        document.getElementById('quickstart-oauthtoken').textContent = 'null';
-    }
-    var user = result.user;
+        if (result.credential) {
+            var token = result.credential.accessToken;
+            document.getElementById('quickstart-oauthtoken').textContent = token;
+        } else {
+            document.getElementById('quickstart-oauthtoken').textContent = 'null';
+        }
+
+        var user = result.user;
     }).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    if (errorCode === 'auth/account-exists-with-different-credential') {
-        alert('You have already signed up with a different auth provider for that email.');
-    } else {
-        console.error(error);
-    }
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+
+        if (errorCode === 'auth/account-exists-with-different-credential') {
+            alert('You have already signed up with a different auth provider for that email.');
+        } else {
+            console.error(error);
+        }
     });
+
     firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-    } else {
-        document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
-    }
-    document.getElementById('quickstart-sign-in').disabled = false;
+        if (user) {
+            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+        } else {
+            document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
+        }
+
+        document.getElementById('quickstart-sign-in').disabled = false;
     });
+
     document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
 }
+
 window.onload = function() {
     initApp();
 };
