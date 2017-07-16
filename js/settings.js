0356675fa02b7556e1990bcb7ca65f47b38bb307
Andre Reus <andrereus@users.noreply.github.com>
2017-07-16 22:59:35 +0200

    Reset app

--- js/settings.js
+++ js/settings.js
@@ -59,13 +59,13 @@ function resetapp() {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             firebase.database().ref(user.uid).once("value").then(function(snapshot) {
-                conf = confirm("Please confirm to reset entries.");
+                conf = confirm("Please confirm to reset app.");
                 firebase.database().ref(user.uid).remove().then(function(error){
                     window.location.assign("index.html");
                 });
             });
         } else {
-            conf = confirm("Please confirm to reset entries.");
+            conf = confirm("Please confirm to reset app.");
             if (conf === true) {
                 localStorage.clear();
                 window.location.assign("index.html");
