0f096c3ca4606ff954d5d8c2472683b06a023cb9
Andre Reus <andrereus@users.noreply.github.com>
2025-07-10 19:51:52 +0200

    Reformat, delete old, remove hotjar & google analytics

--- js/edit.js
+++ js/edit.js
@@ -11,19 +11,23 @@ var drop = document.getElementById("delete");
 var calculate = document.getElementById("calculate");
 
 function addListener(element, event, funct) {
-    if (element.addEventListener) {
-        return element.addEventListener(event, funct);
-    } else if (element.attachEvent) {
-        return element.attachEvent(event, funct);
-    }
+  if (element.addEventListener) {
+    return element.addEventListener(event, funct);
+  } else if (element.attachEvent) {
+    return element.attachEvent(event, funct);
+  }
 }
 
 function addParameter(element, event, funct, p1, p2) {
-    if (element.addEventListener) {
-        return element.addEventListener(event, function () { funct(p1, p2); });
-    } else if (element.attachEvent) {
-        return element.attachEvent(event, function () { funct(p1, p2); });
-    }
+  if (element.addEventListener) {
+    return element.addEventListener(event, function () {
+      funct(p1, p2);
+    });
+  } else if (element.attachEvent) {
+    return element.attachEvent(event, function () {
+      funct(p1, p2);
+    });
+  }
 }
 
 /* Get */
@@ -34,80 +38,96 @@ var save;
 addListener(edit, "click", save);
 
 function save() {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            firebase.database().ref(user.uid).once("value").then(function (snapshot) {
-                var list = snapshot.val();
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      firebase
+        .database()
+        .ref(user.uid)
+        .once("value")
+        .then(function (snapshot) {
+          var list = snapshot.val();
 
-                for (var key in list) {
-                    if (list[key].id == searchId) {
-                        list[key].desc = description.value;
-                        list[key].wg = Number(weight.value);
-                        list[key].phe = Number(phenylalanine.value);
-                        list[key].prot = Number(protein.value);
-                        list[key].kcal = Number(energy.value);
-                    }
-                }
-
-                firebase.database().ref(user.uid).update(list, function (error) {
-                    window.location.assign("index.html");
-                });
-            });
-        } else {
-            var list = JSON.parse(localStorage.getItem("day"));
-
-            for (var i = 0; i < list.length; i++) {
-                if (list[i].id == searchId) {
-                    list[i].desc = description.value;
-                    list[i].wg = Number(weight.value);
-                    list[i].phe = Number(phenylalanine.value);
-                    list[i].prot = Number(protein.value);
-                    list[i].kcal = Number(energy.value);
-                }
+          for (var key in list) {
+            if (list[key].id == searchId) {
+              list[key].desc = description.value;
+              list[key].wg = Number(weight.value);
+              list[key].phe = Number(phenylalanine.value);
+              list[key].prot = Number(protein.value);
+              list[key].kcal = Number(energy.value);
             }
+          }
 
-            localStorage.setItem("day", JSON.stringify(list));
-            window.location.assign("index.html");
+          firebase
+            .database()
+            .ref(user.uid)
+            .update(list, function (error) {
+              window.location.assign("index.html");
+            });
+        });
+    } else {
+      var list = JSON.parse(localStorage.getItem("day"));
+
+      for (var i = 0; i < list.length; i++) {
+        if (list[i].id == searchId) {
+          list[i].desc = description.value;
+          list[i].wg = Number(weight.value);
+          list[i].phe = Number(phenylalanine.value);
+          list[i].prot = Number(protein.value);
+          list[i].kcal = Number(energy.value);
         }
-    });
+      }
+
+      localStorage.setItem("day", JSON.stringify(list));
+      window.location.assign("index.html");
+    }
+  });
 }
 
 /* Delete */
-var remove, dropId
+var remove, dropId;
 addListener(drop, "click", remove);
 
 function remove() {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            firebase.database().ref(user.uid).once("value").then(function (snapshot) {
-                var list = snapshot.val();
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      firebase
+        .database()
+        .ref(user.uid)
+        .once("value")
+        .then(function (snapshot) {
+          var list = snapshot.val();
 
-                for (var key in list) {
-                    if (list[key].id == searchId) {
-                        firebase.database().ref(user.uid).child(key).remove().then(function (error) {
-                            window.location.assign("index.html");
-                        });
-                    }
-                }
-            });
-        } else {
-            var list = JSON.parse(localStorage.getItem("day"));
-
-            for (var i = 0; i < list.length; i++) {
-                if (list[i].id == searchId) {
-                    dropId = i;
-                }
+          for (var key in list) {
+            if (list[key].id == searchId) {
+              firebase
+                .database()
+                .ref(user.uid)
+                .child(key)
+                .remove()
+                .then(function (error) {
+                  window.location.assign("index.html");
+                });
             }
+          }
+        });
+    } else {
+      var list = JSON.parse(localStorage.getItem("day"));
 
-            if (list.length > 1) {
-                list.splice(dropId, 1);
-                localStorage.setItem("day", JSON.stringify(list));
-            } else {
-                localStorage.removeItem("day");
-            }
-            window.location.assign("index.html");
+      for (var i = 0; i < list.length; i++) {
+        if (list[i].id == searchId) {
+          dropId = i;
         }
-    });
+      }
+
+      if (list.length > 1) {
+        list.splice(dropId, 1);
+        localStorage.setItem("day", JSON.stringify(list));
+      } else {
+        localStorage.removeItem("day");
+      }
+      window.location.assign("index.html");
+    }
+  });
 }
 
 /* Calculate */
@@ -119,21 +139,35 @@ addParameter(protein, "keyup", calc, protein, "protein");
 addParameter(energy, "keyup", calc, energy, "energy");
 
 function check() {
-    if (calculate.checked) {
-        localStorage.setItem("weight", weight.value);
-        localStorage.setItem("phenylalanine", phenylalanine.value);
-        localStorage.setItem("protein", protein.value);
-        localStorage.setItem("energy", energy.value);
-    }
+  if (calculate.checked) {
+    localStorage.setItem("weight", weight.value);
+    localStorage.setItem("phenylalanine", phenylalanine.value);
+    localStorage.setItem("protein", protein.value);
+    localStorage.setItem("energy", energy.value);
+  }
 }
 
 function calc(original, saved) {
-    if (calculate.checked && original.value.length !== 0) {
-        weight.value = (original.value * localStorage.getItem("weight") / localStorage.getItem(saved)).toFixed(2).replace(/\.?0+$/, "");
-        phenylalanine.value = (original.value * localStorage.getItem("phenylalanine") / localStorage.getItem(saved)).toFixed(2).replace(/\.?0+$/, "");
-        protein.value = (original.value * localStorage.getItem("protein") / localStorage.getItem(saved)).toFixed(2).replace(/\.?0+$/, "");
-        energy.value = (original.value * localStorage.getItem("energy") / localStorage.getItem(saved)).toFixed(2).replace(/\.?0+$/, "");
-    }
+  if (calculate.checked && original.value.length !== 0) {
+    weight.value = ((original.value * localStorage.getItem("weight")) / localStorage.getItem(saved))
+      .toFixed(2)
+      .replace(/\.?0+$/, "");
+    phenylalanine.value = (
+      (original.value * localStorage.getItem("phenylalanine")) /
+      localStorage.getItem(saved)
+    )
+      .toFixed(2)
+      .replace(/\.?0+$/, "");
+    protein.value = (
+      (original.value * localStorage.getItem("protein")) /
+      localStorage.getItem(saved)
+    )
+      .toFixed(2)
+      .replace(/\.?0+$/, "");
+    energy.value = ((original.value * localStorage.getItem("energy")) / localStorage.getItem(saved))
+      .toFixed(2)
+      .replace(/\.?0+$/, "");
+  }
 }
 
 /* Estimate */
@@ -142,45 +176,49 @@ addListener(phenylalanine, "blur", estimate);
 addListener(protein, "blur", estimate);
 
 function estimate() {
-    if (phenylalanine.value.length !== 0 && protein.value.length === 0) {
-        protein.value = (phenylalanine.value / 50).toFixed(2).replace(/\.?0+$/, "");
-    } else if (protein.value.length !== 0 && phenylalanine.value.length === 0) {
-        phenylalanine.value = (protein.value * 50).toFixed(2).replace(/\.?0+$/, "");
-    }
+  if (phenylalanine.value.length !== 0 && protein.value.length === 0) {
+    protein.value = (phenylalanine.value / 50).toFixed(2).replace(/\.?0+$/, "");
+  } else if (protein.value.length !== 0 && phenylalanine.value.length === 0) {
+    phenylalanine.value = (protein.value * 50).toFixed(2).replace(/\.?0+$/, "");
+  }
 }
 
 /* Grab */
 firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        firebase.database().ref(user.uid).once("value").then(function (snapshot) {
-            var list = snapshot.val();
+  if (user) {
+    firebase
+      .database()
+      .ref(user.uid)
+      .once("value")
+      .then(function (snapshot) {
+        var list = snapshot.val();
 
-            for (var key in list) {
-                if (list[key].id == searchId) {
-                    description.value = list[key].desc;
-                    weight.value = list[key].wg.toFixed(2).replace(/\.?0+$/, "");
-                    phenylalanine.value = list[key].phe.toFixed(2).replace(/\.?0+$/, "");
-                    protein.value = list[key].prot.toFixed(2).replace(/\.?0+$/, "");
-                    energy.value = list[key].kcal.toFixed(2).replace(/\.?0+$/, "");
-                }
-            }
-        });
-    } else {
-        var list = JSON.parse(localStorage.getItem("day"));
-
-        for (var i = 0; i < list.length; i++) {
-            if (list[i].id == searchId) {
-                description.value = list[i].desc;
-                weight.value = list[i].wg.toFixed(2).replace(/\.?0+$/, "");
-                phenylalanine.value = list[i].phe.toFixed(2).replace(/\.?0+$/, "");
-                protein.value = list[i].prot.toFixed(2).replace(/\.?0+$/, "");
-                energy.value = list[i].kcal.toFixed(2).replace(/\.?0+$/, "");
-            }
+        for (var key in list) {
+          if (list[key].id == searchId) {
+            description.value = list[key].desc;
+            weight.value = list[key].wg.toFixed(2).replace(/\.?0+$/, "");
+            phenylalanine.value = list[key].phe.toFixed(2).replace(/\.?0+$/, "");
+            protein.value = list[key].prot.toFixed(2).replace(/\.?0+$/, "");
+            energy.value = list[key].kcal.toFixed(2).replace(/\.?0+$/, "");
+          }
         }
+      });
+  } else {
+    var list = JSON.parse(localStorage.getItem("day"));
+
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].id == searchId) {
+        description.value = list[i].desc;
+        weight.value = list[i].wg.toFixed(2).replace(/\.?0+$/, "");
+        phenylalanine.value = list[i].phe.toFixed(2).replace(/\.?0+$/, "");
+        protein.value = list[i].prot.toFixed(2).replace(/\.?0+$/, "");
+        energy.value = list[i].kcal.toFixed(2).replace(/\.?0+$/, "");
+      }
     }
+  }
 });
 
 /* Hide */
 if (localStorage.getItem("hide") === "true") {
-    $(".energy").addClass("visuallyhidden");
+  $(".energy").addClass("visuallyhidden");
 }
--- js/login.js
+++ js/login.js
@@ -2,15 +2,15 @@
  * Function called when clicking the Login/Logout button.
  */
 function toggleSignIn() {
-    if (!firebase.auth().currentUser) {
-        var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithRedirect(provider);
-    } else {
-        firebase.auth().signOut();
-        window.location.href = "index.html";
-    }
+  if (!firebase.auth().currentUser) {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+  } else {
+    firebase.auth().signOut();
+    window.location.href = "index.html";
+  }
 
-    // document.getElementById('sign-in').classList.add('disabled');
+  // document.getElementById('sign-in').classList.add('disabled');
 }
 
 /**
@@ -21,51 +21,56 @@ function toggleSignIn() {
  *    the auth redirect flow. It is where you can get the OAuth access token from the IDP.
  */
 function initApp() {
-    // Result from Redirect auth flow.
-    firebase.auth().getRedirectResult().then(function (result) {
-        if (result.credential) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-        }
+  // Result from Redirect auth flow.
+  firebase
+    .auth()
+    .getRedirectResult()
+    .then(function (result) {
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+      }
 
-        // The signed-in user info.
-        var user = result.user;
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+      // The signed-in user info.
+      var user = result.user;
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
 
-        if (errorCode === 'auth/account-exists-with-different-credential') {
-            alert('You have already signed up with a different auth provider for that email.');
-            // If you are using multiple auth providers on your app you should handle linking
-            // the user's accounts here.
-        } else {
-            console.error(error);
-        }
+      if (errorCode === "auth/account-exists-with-different-credential") {
+        alert("You have already signed up with a different auth provider for that email.");
+        // If you are using multiple auth providers on your app you should handle linking
+        // the user's accounts here.
+      } else {
+        console.error(error);
+      }
     });
 
-    // Listening for auth state changes.
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // User is signed in.
-            document.getElementById('storage').innerHTML = '<i class="fas fa-user"></i> ' + user.displayName + ' <i class="fas fa-angle-down"></i>';
-            // document.getElementById('sign-in').innerHTML = '<i class="fas fa-sign-out-alt"></i> Sign out';
-        } else {
-            // User is signed out.
-            document.getElementById('storage').innerHTML = 'Menu <i class="fas fa-angle-down"></i>';
-            // document.getElementById('sign-in').innerHTML = '<i class="fas fa-sign-in-alt"></i> Sign in with Google';
-        }
+  // Listening for auth state changes.
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+      document.getElementById("storage").innerHTML =
+        '<i class="fas fa-user"></i> ' + user.displayName + ' <i class="fas fa-angle-down"></i>';
+      // document.getElementById('sign-in').innerHTML = '<i class="fas fa-sign-out-alt"></i> Sign out';
+    } else {
+      // User is signed out.
+      document.getElementById("storage").innerHTML = 'Menu <i class="fas fa-angle-down"></i>';
+      // document.getElementById('sign-in').innerHTML = '<i class="fas fa-sign-in-alt"></i> Sign in with Google';
+    }
 
-        // document.getElementById('sign-in').classList.remove('disabled');
-    });
+    // document.getElementById('sign-in').classList.remove('disabled');
+  });
 
-    // document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
+  // document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
 }
 
 window.onload = function () {
-    initApp();
+  initApp();
 };
